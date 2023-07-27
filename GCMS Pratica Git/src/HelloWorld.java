cf0abdc4b6d7128ca958388e20a2062e8ef9851b
Anderson Guilherme <Anderson Guilherme@DESKTOP-Q6NIBUR>
2023-07-27 20:27:43 -0300

    Primeiro Commit

---
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+	static public void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.println("cavalo de troia");
+		int a = 56;
+		int b = 67;
+		System.out.println(a+b);
+		
+	}
+}
